d961327ec33076e6d3ed9e6dfef5c36fe62bbc6b
mmcentre <michael.bouzinier@kvitneset.com>
2021-11-13 19:02:22 -0500

    MAB    Adding DAG fro Medicaid

---
+++ project/examples/medicaid.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from cwl_airflow.extensions.cwldag import CWLDAG
+
+args = {
+    "cwl": {
+        "debug": True,
+        "parallel": True
+    }
+}
+
+dag = CWLDAG(
+    workflow="/opt/airflow/project/cms/src/cwl/medicaid.cwl",
+    dag_id="pi",
+    default_args=args
+)
