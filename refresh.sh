35f187f8024302b217e08fa56095d290de0d05d8
mmcentre <michael.bouzinier@kvitneset.com>
2021-11-18 01:56:15 -0500

    MAB    Adding DAG fro Medicaid

---
+++ refresh.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git pull
+git submodule update --recursive
+cd project/ || exit
+for d in nsaph_utils data_platform epa cms census ; do
+  pushd $d && git status && git pull && popd || exit ;
+done
+cd ..
+
