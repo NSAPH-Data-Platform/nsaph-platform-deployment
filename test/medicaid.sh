b6bf9000bc1383565dc1dea915275f2303c2b20b
mmcentre <michael.bouzinier@kvitneset.com>
2021-11-20 14:41:17 -0500

    MAB    Mapping cwl-rundir between host and containers

--- test/medicaid.sh
+++ test/medicaid.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker exec  webserver bash -c "source /root/anaconda/etc/profile.d/conda.sh && conda activate nsaph && cd /root/tmp && cwl-runner /opt/airflow/project/cms/src/cwl/medicaid.cwl --database /opt/airflow/project/database.ini --connection_name nsaph2 --input /data/incoming/medicaid/random_data/ "
+rundir=/opt/airflow/cwl-rundir/medicaid/run-`date +%Y-%m-%d-%H-%M`
+docker exec  webserver bash -c "source /root/anaconda/etc/profile.d/conda.sh && conda activate nsaph && mkdir -p ${rundir} && cd ${rundir} && cwl-runner /opt/airflow/project/cms/src/cwl/medicaid.cwl --database /opt/airflow/project/database.ini --connection_name nsaph2 --input /data/incoming/medicaid/random_data/ "
+
