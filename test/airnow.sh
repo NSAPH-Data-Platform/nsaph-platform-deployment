b3279a47e688c1082a6c2884f88f9d63a18b9332
mmcentre <michael.bouzinier@kvitneset.com>
2021-11-23 17:15:31 -0500

    MAB    Configuring EPA pipelines in Airflow: AQS DAG and job file

---
+++ test/airnow.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rundir=/opt/airflow/cwl-rundir/airnow/run-`date +%Y-%m-%d-%H-%M`
+docker exec  webserver bash -c "source /root/anaconda/etc/profile.d/conda.sh && conda activate nsaph && mkdir -p ${rundir} && cd ${rundir} && cwl-runner /opt/airflow/project/epa/src/cwl/airnow.cwl --database /opt/airflow/project/database.ini --connection_name nsaph2 --parameter_code PM25 --table pm25_airnow_test --proxy $HTTP_PROXY --api-key 9B053C38-3C42-416E-A330-203A698CCCDA --from 2020-12-25 --to 2021-01-05 "
