1143d0e5c21551366fd3d015bb0027785f37b8ee
mmcentre <michael.bouzinier@kvitneset.com>
2021-12-06 19:54:50 -0500

    MAB    convenience scripts

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export log=build-`date +%Y-%m-%d-%H-%M`.log && date > $log && cat .env >> $log && DOCKER_BUILDKIT=1 BUILDKIT_PROGRESS=plain docker-compose --env-file ./.env  build --no-cache 2>&1 | tee -a $log && date >> $log
+./hardreset.sh
 mkdir -p ./dags && cp -rf ./project/examples/* ./dags
 docker-compose --env-file ./.env up
---
+++ hardreset.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker-compose down &&  docker system prune -a -y && export log=build-`date +%Y-%m-%d-%H-%M`.log && date > $log && cat .env >> $log && DOCKER_BUILDKIT=1 BUILDKIT_PROGRESS=plain docker-compose --env-file ./.env  build --no-cache 2>&1 | tee -a $log && date >> $log
