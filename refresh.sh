80975da9477678f9807b6cd9a51da175ae3a5178
mmcentre <michael.bouzinier@kvitneset.com>
2021-12-09 17:58:01 -0500

    MAB    Adding gridMET

--- refresh.sh
+++ refresh.sh
@@ -3,7 +3,7 @@
 git pull
 # git submodule update --recursive
 cd project/ || exit
-for d in nsaph_utils data_platform epa cms census ; do
+for d in nsaph_utils data_platform epa cms census gridmet ; do
   pushd $d && git status && git pull && popd || exit ;
 done
 cd ..
