131c58ca8bc057d79dfb69fc4c24387e972a46ab
mmcentre <michael.bouzinier@kvitneset.com>
2022-06-08 13:56:29 -0400

    MAB   Refresh: checkout develop

--- refresh.sh
+++ refresh.sh
@@ -4,7 +4,7 @@ git pull
 # git submodule update --recursive
 cd project/ || exit
 for d in nsaph_utils data_platform gis epa cms census gridmet ; do
-  pushd $d && git status && git checkout develop && git pull && docker exec  webserver bash -c "source /root/anaconda/etc/profile.d/conda.sh && conda activate nsaph && pip install /opt/airflow/project/$d" && popd || exit ;
+  pushd $d && git status && git checkout develop && git pull && docker exec  webserver bash -c "source /root/anaconda/etc/profile.d/conda.sh && conda activate nsaph && cd /opt/airflow/project/$d && pip install ." && popd || exit ;
 done
 cd ..
 
