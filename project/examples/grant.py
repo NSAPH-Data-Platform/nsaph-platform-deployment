270d0345c089154e47bcb213341beea0d3e6c07b
mmcentre <michael.bouzinier@kvitneset.com>
2022-10-28 16:07:53 -0400

    MAB   Adding GRANT DAG

--- project/examples/grant.py
+++ project/examples/grant.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
+
+"""
+Execution of this DAG will grant `SELECT` privileges to all
+tables and
+NSAPH group users. By default it is executed in the production
+database.
+
+A different database can be selected by overriding
+`connection_name` parameter in the JSON Job COnfiguration
+"""
+
 from cwl_airflow.extensions.cwldag import CWLDAG
 
 args = {
@@ -10,5 +21,16 @@ args = {
 dag = CWLDAG(
     workflow="/opt/airflow/project/cms/src/cwl/grant.cwl",
     dag_id="Grant_Access_to_Tables",
+    params = {
+        "job": {
+            "database": {
+              	"class": "File",
+          		"path": "/opt/airflow/project/database.ini"
+            },
+            "connection_name": "nsaph2"
+        }
+    },
+    doc_md = __doc__,
+    description="Grant Read access to teh database",
     default_args=args
 )
